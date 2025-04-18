f110c9abc737edeae4eb9dbed2ef4aae184d7049
programmercito <aps@outlook.com>
2025-04-18 19:23:53 -0400

    Implement Telegram bot functionality in DatesBotApplication

--- src/main/java/org/osbo/bots/DatesBotApplication.java
+++ src/main/java/org/osbo/bots/DatesBotApplication.java
@@ -1,12 +1,37 @@
 package org.osbo.bots;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+
 @SpringBootApplication
 public class DatesBotApplication {
+	@Value("${telegram.token}")
+	private static String token;
 
 	public static void main(String[] args) {
+		TelegramBot bot = new TelegramBot(token);
+		bot.setUpdatesListener(updates -> {
+			for (var update : updates) {
+				if (update.message() != null) {
+					// handle message
+					System.out.println(update.message().text());
+				}
+			}
+			return UpdatesListener.CONFIRMED_UPDATES_ALL;
+		}, e -> {
+			if (e.response() != null) {
+				// got bad response from telegram
+				e.response().errorCode();
+				e.response().description();
+			} else {
+				// probably network error
+				e.printStackTrace();
+			}
+		});
 		SpringApplication.run(DatesBotApplication.class, args);
 	}
 
