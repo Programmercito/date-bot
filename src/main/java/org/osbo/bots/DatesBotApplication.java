39b9c4dc8cd6cd4d0d34e00e8ddba3de2cc41d89
programmercito <aps@outlook.com>
2025-04-18 17:39:56 -0400

    first commit

---
+++ src/main/java/org/osbo/bots/DatesBotApplication.java
@@ -0,0 +1,13 @@
+package org.osbo.bots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatesBotApplication.class, args);
+	}
+
+}
