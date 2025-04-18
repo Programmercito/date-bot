d903b7de8661fc8feb2c0f87709badcd442c2c45
programmercito <aps@outlook.com>
2025-04-18 19:35:17 -0400

    Implement Telegram bot initialization and update message processing in StartupRunner

--- src/main/java/org/osbo/bots/runner/StartupRunner.java
+++ src/main/java/org/osbo/bots/runner/StartupRunner.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.UpdatesListener;
+
 @Component
 public class StartupRunner implements CommandLineRunner {
     @Value("${telegram.token}")
@@ -12,6 +15,31 @@ public class StartupRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // Código que se ejecuta al iniciar la app
-        System.out.println("La aplicación se ha iniciado." + telegramToken);
+        TelegramBot bot = new TelegramBot(telegramToken);
+        bot.setUpdatesListener(updates -> {
+            // ... process updates
+            for (var update : updates) {
+                // process each update
+                if (update.message() != null) {
+                    // process message
+                    System.out.println("Received message: " + update.message().text());
+                } else if (update.callbackQuery() != null) {
+                    // process callback query
+                    System.out.println("Received callback query: " + update.callbackQuery().data());
+                }
+            }
+            // return id of last processed update or confirm them all
+            return UpdatesListener.CONFIRMED_UPDATES_ALL;
+            // Create Exception Handler
+        }, e -> {
+            if (e.response() != null) {
+                // got bad response from telegram
+                e.response().errorCode();
+                e.response().description();
+            } else {
+                // probably network error
+                e.printStackTrace();
+            }
+        });
     }
 }
