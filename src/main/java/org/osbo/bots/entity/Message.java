1afed3e8c2f123910c065e6cc90093e1286d1694
programmercito <aps@outlook.com>
2025-04-19 15:52:34 -0400

    Add Lombok dependency and create Message and User entity classes for database mapping

---
+++ src/main/java/org/osbo/bots/entity/Message.java
@@ -0,0 +1,20 @@
+package org.osbo.bots.entity;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "messages")
+public class Message {
+    @Id
+    private String messageid;
+    private String texto;
+    private String userid;
+    private String expiracion;
+    private String estado;
+
+}
