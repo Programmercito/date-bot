fa20521a72cda601cdc84cd39cdd41256ea85470
programmercito <aps@outlook.com>
2025-04-19 17:57:29 -0400

    Refactor UserService and MessageProcessor for improved code structure and readability

--- src/main/java/org/osbo/bots/model/services/UserService.java
+++ src/main/java/org/osbo/bots/model/services/UserService.java
@@ -4,17 +4,29 @@ import org.osbo.bots.model.entity.User;
 import org.osbo.bots.model.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class UserService {
     private UserRepository userRepository;
+
     UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
-    public User save(User user){
-       return userRepository.save(user);
+
+    public User save(User user) {
+        return userRepository.save(user);
     }
-    public User findById(String id){
+
+    public User findById(String id) {
         return userRepository.findById(id).orElse(null);
+    }
+
+    public User Persiste(User user) {
+        User existingUser = userRepository.findById(user.getChatid()).orElse(null);
+        if (existingUser != null) {
+            return existingUser;
+        } else {
+            user.setEstado("ACTIVO");
+            return userRepository.save(user);
+        }
     }
 }
--- src/main/java/org/osbo/bots/processor/MessageProcessor.java
+++ src/main/java/org/osbo/bots/processor/MessageProcessor.java
@@ -3,21 +3,24 @@ package org.osbo.bots.processor;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
+
+import org.osbo.bots.model.services.UserService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageProcessor {
-    private final TelegramBot bot;
-
-    public MessageProcessor(TelegramBot bot) {
-        this.bot = bot;
+    private UserService userService;
+    MessageProcessor(UserService userService) {
+        this.userService = userService;
     }
+    private TelegramBot bot;
 
     public void process(Update update) {
         if (update.message() != null) {
             String text = update.message().text();
             Long chatId = update.message().chat().id();
             if ("/start".equals(text)) {
+
                 String welcome = "\uD83D\uDC65 ¡Bienvenido al Bot de Anuncios de Amistad! \uD83D\uDC65\n\n" +
                         "Este bot te permite publicar anuncios de amistad en nuestro canal de Telegram. " +
                         "\n\nEscribe tu anuncio y yo lo enviaré al canal para que otros puedan conocerte.\n" +
